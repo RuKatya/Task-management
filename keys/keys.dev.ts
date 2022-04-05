cc96045717d9ad68350169ca681b7faca9b8400b
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-04-06 00:07:06 +0300

    task style

--- keys/keys.dev.ts
+++ keys/keys.dev.ts
@@ -1,7 +1,7 @@
 module.exports = {
     MONGODB_URI: `mongodb+srv://KaKa:WO3sm2Hl7eXTMN0Y@cluster0.mfqlq.mongodb.net/house`,
     SESSION_SECRET: 'some secret value',
-    EMAIL_USER: 'task.menagment@gmail.com',
-    EMAIL_FROM: 'task.menagment@gmail.com',
-    EMAIL_PASS: 't@$kmenagment'
+    // EMAIL_USER: 'task.menagment@gmail.com',
+    // EMAIL_FROM: 'task.menagment@gmail.com',
+    // EMAIL_PASS: 't@$kmenagment'
 };
